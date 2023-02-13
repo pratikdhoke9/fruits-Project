b20e5ea3fb13cb320a1ab4a45f19eeefeaf5b1f8
Pratik Dhoke <pratikdhoke9@gmail.com>
2023-02-13 12:18:18 +0530

    data added

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const mongoose = require('mongoose')
 const express = require("express")
 const app= express()
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/fruitDb',
+  {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 const fruitSchema = new mongoose.Schema({
   name: String,
